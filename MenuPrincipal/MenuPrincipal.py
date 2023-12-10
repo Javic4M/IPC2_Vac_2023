ee71065a228b4e069246dcddde3c4f32602ef2cd
Javic4M <100719193+Javic4M@users.noreply.github.com>
2023-12-09 20:13:18 -0600

    Add files via upload

--- MenuPrincipal/MenuPrincipal.py
+++ MenuPrincipal/MenuPrincipal.py
@@ -4,10 +4,6 @@ from dataclasses import dataclass
 
 @dataclass
 class Cancion:
-    nombre = ""
-    ruta = ""
-    imagen = ""
-    numero: int
     
     def __init__(self, nombre, ruta, imagen, numero):
         self.nombre = nombre
@@ -62,42 +58,44 @@ class Lista:
         self._longitud += 1
            
     def eliminarDeLaLista(self, indice: int):
-        actual = self._inicio
-
-        if (self._longitud == 1 and indice == 1):
-            self._inicio = self._ultimo = nullcontext
-            
-        elif (self._longitud != 1 and indice == 1):
-            print("Opción 2")
-            self._inicio = self._inicio._nodoSiguiente
-            self._inicio.guardarAnterior(nullcontext)
-            
-        elif (indice == self._longitud):
-            print("Opción 3")
-            self._ultimo = self._ultimo.obtenerAnterior()
-            self._ultimo.guardarSiguiente(nullcontext)
-            
-        else:
-            print("Opción 4")
-            aEliminar: 'Nodo'
-            rango = self._longitud / 2
-                
-            if (indice >= rango):
-                indice_1 = self._longitud - indice
-                print("NUmero: " + str(indice_1))
-                aEliminar = self.encontrarPorIndiceFinalInicio(indice_1)
-            else:
-                aEliminar = self.encontrarPorIndiceInicioFinal(indice - 1)
-                                
-            print("N............" + aEliminar._cancion.nombre)
-            anterior = aEliminar.obtenerAnterior()
-            siguiente = aEliminar.obtenerSiguiente()
-            anterior.guardarSiguiente(siguiente)
-            siguiente.guardarAnterior(anterior)
-            aEliminar.guardarSiguiente(nullcontext)
-            aEliminar.guardarAnterior(nullcontext)         
-        self._longitud -= 1    
         
+        if (self.estaVacia()):
+            print("\nLa Lista esta Vacia\n")
+        else:
+            if (indice >= 1 and indice <= self._longitud):
+                actual = self._inicio
+
+                if (self._longitud == 1 and indice == 1):
+                    self._inicio = self._ultimo = nullcontext
+                    
+                elif (self._longitud != 1 and indice == 1):
+                    self._inicio = self._inicio._nodoSiguiente
+                    self._inicio.guardarAnterior(nullcontext)
+                    
+                elif (indice == self._longitud):
+                    self._ultimo = self._ultimo.obtenerAnterior()
+                    self._ultimo.guardarSiguiente(nullcontext)
+                    
+                else:
+                    aEliminar: 'Nodo'
+                    rango = self._longitud / 2
+                        
+                    if (indice >= rango):
+                        indice_1 = self._longitud - indice
+                        aEliminar = self.encontrarPorIndiceFinalInicio(indice_1)
+                    else:
+                        aEliminar = self.encontrarPorIndiceInicioFinal(indice - 1)
+                                        
+                    anterior = aEliminar.obtenerAnterior()
+                    siguiente = aEliminar.obtenerSiguiente()
+                    anterior.guardarSiguiente(siguiente)
+                    siguiente.guardarAnterior(anterior)
+                    aEliminar.guardarSiguiente(nullcontext)
+                    aEliminar.guardarAnterior(nullcontext)         
+                self._longitud -= 1    
+            else:
+                print("\nIndice Fuera de Rango\n")
+            
     def encontrarPorIndiceInicioFinal(self, indice): 
         actual = self._inicio
         
@@ -115,40 +113,61 @@ class Lista:
         return actual
     
     def obtenerContenido(self, indice):
-        contenido: 'Nodo'
         
-        rango = self._longitud / 2
-        
-        if (indice >= rango):
-            indice_1 = self._longitud - indice
-            contenido = self.encontrarPorIndiceFinalInicio(indice_1)
+        if (self.estaVacia()):
+            print("\nLa Lista esta Vacia\n")
         else:
-            contenido = self.encontrarPorIndiceInicioFinal(indice - 1)
+            if (indice >= 1 and indice <= self._longitud):
+                contenido: 'Nodo'       
+                rango = self._longitud / 2
                 
-        return contenido._cancion
+                if (indice >= rango):
+                    indice_1 = self._longitud - indice
+                    contenido = self.encontrarPorIndiceFinalInicio(indice_1)
+                else:
+                    contenido = self.encontrarPorIndiceInicioFinal(indice - 1)    
+                return contenido._cancion
+            else:
+                print("\nIndice fuera de Rango\n")
+    
+    def estaVacia(self):
+        return self._longitud == 0
+        
         
 class MenuPrincipal:
     
     lista = Lista()
-    lista.agregarALaLista(Cancion("Navidad", "J", "C", 0))
-    lista.agregarALaLista(Cancion("Año_Nuevo", "J", "C", 1))
-    lista.agregarALaLista(Cancion("Felicidades", "J", "C", 2))
+    #lista.agregarALaLista(Cancion("Noche de Paz", "ruta_1", "noche.png", 1))
+    #lista.agregarALaLista(Cancion("Feliz Navidad", "ruta_2", "felizNavidad.png", 2))
+    lista.agregarALaLista(Cancion("Rodolfo el Reno", "ruta_3", "reno.png", 3))
+    
     opcion = int(input("Menu Principal \n    1. Agregar a la Lista \n    2. Listar \n    3. Listar Uno \n    4. Eliminar \n    5. Salir \n"))
 
     while opcion != 5:
             
         if (opcion == 1):
-            lista.agregarALaLista(Cancion("Javier", "J", "C", 3))
+            lista.agregarALaLista(Cancion(input("Ingrese el Nombre de la Canción: "), "ruta_" + str(lista._longitud + 1), "img.png", lista._longitud + 1))
+            print("\n-----------------------------")
+            print("Canción Ingresada Correctamente")
+            print("-----------------------------\n") 
         elif (opcion == 2):
-            print("Longitud: " + str(lista._longitud))   
+            print("\n-------------------------")
             for i in range(lista._longitud):
-                contenido = lista.obtenerContenido(i + 1)
-                print("Cancion: " + contenido.nombre)                  
+                contenido = lista.obtenerContenido(i + 1) 
+                print("Canción: " + contenido.nombre)    
+            print("-------------------------\n")                 
         elif (opcion == 3):
-            contenido = lista.obtenerContenido(int(input("Ingrese Indice_1: ")))
-            print("- Cancion: " + contenido.nombre) 
+            try:
+                contenido = lista.obtenerContenido(int(input("Ingrese el Número de la Canción que desea Reproducir: ")))
+                if (contenido != None):
+                    print("\n---------------------\nCanción \n  Nombre: " + contenido.nombre + "\n  Ruta: " + contenido.ruta + "\n  Numero: " + str(contenido.numero) + "\n---------------------\n")                             
+            except ValueError:
+                print("\n---------------------\nDebe Ingresar un Número\n---------------------\n") 
         elif (opcion == 4):
-            lista.eliminarDeLaLista(int(input("Ingrese Indice_2: ")))
+            try:
+                lista.eliminarDeLaLista(int(input("Ingrese la Canción que desea Eliminar: ")))
+            except ValueError:
+                print("\n---------------------\nDebe Ingresar un Número\n---------------------\n") 
             
         opcion = int(input("Menu Principal \n    1. Agregar a la Lista \n    2. Listar \n    3. Listar Uno \n    4. Eliminar \n    5. Salir \n"))
   
