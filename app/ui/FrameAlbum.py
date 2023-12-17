b87f32bd0242479115211bcd84017d793cbf87a3
Jbrod5 <jorgebravo202131782@cunoc.edu.gt>
2023-12-17 03:39:03 -0600

    candidato a FrameAlbum

--- app/ui/FrameAlbum.py
+++ app/ui/FrameAlbum.py
@@ -22,22 +22,45 @@ class FrameAlbum(tk.Frame):
         self.biblioteca = biblioteca
         
         # Titulo del album --------------------------------
-        self.frame_titulo = tk.Frame(bg = "black")
-        # Crear imagen del album
-        self.portada = self.obtener_imagen_album()
-        self.label_portada = tk.Label(self, image = self.portada)
-        self.label_portada.grid(row = 1, column = 0, padx = 10, pady = 20)
+        self.frame_titulo = tk.Frame(self, bg="black")
+        self.frame_titulo.grid(row=0, column=0, sticky="ew")
+        self.frame_titulo.grid_propagate(False)  # Evitar que el marco se ajuste al tamaño de su contenido
+        self.frame_titulo.config(width=900, height=100)
         
-        # Crear label con el nombre del album
+        # Crear imagen del álbum
+        self.portada = self.obtener_imagen_album()
+        self.label_portada = tk.Label(self.frame_titulo, image=self.portada, bg="black")
+        self.label_portada.grid(row=0, column=0, padx=10, pady=5)
+        
+        # Crear label con el nombre del álbum
         self.nombre_album = tk.StringVar()
         self.obtener_nombre_album()
-        self.label_nombre_album = tk.Label(self, textvariable=self.nombre_album, font = ("Arial", 35, "bold"), bg = "black", fg="white")
-        self.label_nombre_album.grid(row = 1, column = 1, padx=10, pady = 30)
+        self.label_nombre_album = tk.Label(self.frame_titulo, textvariable=self.nombre_album, font=("Arial", 35, "bold"), bg="black", fg="white")
+        self.label_nombre_album.grid(row=0, column=1, padx=10, pady=5)
         
-        #Crear boton para reproducir el album
+        # Crear botón para reproducir el álbum
         self.imagen_play = self.obtener_imagen_play()
-        self.boton_reproducir = tk.Button(self, image = self.imagen_play, command = self.reproducir, bg = "black", relief="flat", bd = 0, highlightthickness=0)
-        self.boton_reproducir.grid(row = 1, column = 2, padx=15, pady=30, sticky="e")
+        self.boton_reproducir = tk.Button(self.frame_titulo, image=self.imagen_play, command=self.reproducir, bg="black", relief="flat", bd=0, highlightthickness=0)
+        self.boton_reproducir.grid(row=0, column=2, padx=15, pady=5, sticky="e")
+        
+        # Lista de canciones ----------------------------------------------------------------
+        # Crear un canvas scrollable para la lista de canciones
+        self.canvas = tk.Canvas(self, bg="black")
+        self.canvas.grid(row=1, column=0, sticky="nsew")
+        
+        # Agregar un scrollbar para el canvas
+        scrollbar = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        scrollbar.grid(row=1, column=1, sticky="ns")
+        self.canvas.configure(yscrollcommand=scrollbar.set)
+        
+        # Crear un frame para contener la lista de canciones dentro del canvas
+        self.frame_canciones = tk.Frame(self.canvas, bg="black")
+        self.canvas.create_window((0, 0), window=self.frame_canciones, anchor="nw")
+        
+        # Configurar el canvas para que se expanda con el frame de canciones
+        self.frame_canciones.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+        self.agregar_canciones()
+        
         
     def mostrar_frame(self):
         self.master.cambiar_vista(self)
@@ -82,4 +105,24 @@ class FrameAlbum(tk.Frame):
         self.master.actualizar_informacion_reproductor()
         
     def agregar_canciones(self):
-        
+        lista_canciones = self.album.obtenerListaCanciones()
+        longitud = lista_canciones.obtenerLongitud()
+        contador = 1
+        while contador <= longitud:
+            cancion = lista_canciones.obtenerPorIndice(contador)
+            if cancion != None: 
+                nombre = cancion.obtenerNombre()
+                if nombre == None or nombre == "":
+                    nombre = "Cancion desconocida"
+                    print (nombre)
+                print(nombre)
+                    
+                boton = tk.Button(self.frame_canciones, text=nombre, font = ("Arial", 16), bg = "black", fg = "white", border= 0, highlightthickness= 0, relief = "flat", bd = 0)
+                boton.pack(side = "top", padx = 20, pady = 80, fill = "x", expand=True)
+                
+                            
+            contador += 1
+    
+    
+    def agregar_a_lista(self):
+        a = 10
